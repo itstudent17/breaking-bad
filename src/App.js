f9d99530013733af5e7f9fe6d97d9407c0c09f3e
itstudent17 <itstudent17@outlook.com>
2020-07-03 20:07:24 +0300

    Header component imported

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from "./component/ui/Header";
 
 const App = () => {
   return (
     <div className="container">
-        Hello World!
+        <Header />
     </div>
   );
 }
