be0fee34683469ced098697895f6c66b97b60e53
itstudent17 <itstudent17@outlook.com>
2020-07-03 20:32:52 +0300

    Fetching data is done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 import Header from "./component/ui/Header";
 
 const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios.get(
+        `https://www.breakingbadapi.com/api/characters`
+      );
+      setItems(result.data);
+      setItems(false);
+    };
+    fetchItems();
+  }, []);
+
   return (
     <div className="container">
-        <Header />
+      <Header />
     </div>
   );
-}
+};
 
 export default App;
